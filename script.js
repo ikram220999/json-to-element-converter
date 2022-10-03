78e9274caa629e122a34e281c20b6374b4ff5fb6
Muhammad Ikram bin Azhar <85838927+ikram220999@users.noreply.github.com>
2022-10-03 12:58:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// DELCARE GLOBAL VARIABLE
 let marginTop = 0;
 let marginLeft = 0;
 
@@ -8,6 +7,8 @@ let parentMgLeft = 0;
 function jsonsubmit() {
   let a = document.getElementById("input").value;
   let json = JSON.parse(a);
+
+  console.log("KETUA JSON", json);
   let length = Object.keys(json);
 
   Object.entries(json).forEach((element, idx) => {
@@ -26,9 +27,11 @@ function jsonsubmit() {
       marginLeft = 0;
     }
     if (Array.isArray(element)) {
+      // confirm array
       element.map((ele) => {
-        mgTop = parentMgTop + 20;
+        mgTop = parentMgTop;
         mgLeft = parentMgLeft + 20;
+        parentMgLeft = mgLeft;
 
         addArray(ele, mgTop, mgLeft);
 
@@ -44,16 +47,79 @@ function parentBox(val, mgTop, mgLeft) {
   createEle("grey", val, mgTop, mgLeft);
 }
 
+// ---------------- ALGORITHM ----------------
 function addArray(ele, mgTop, mgLeft) {
   console.log("ele", ele);
   console.log("ele", typeof ele);
   console.log("------------------------");
   if (typeof ele === "object") {
     if (Array.isArray(ele)) {
+      console.log("inner 1", ele);
       ele.map((ele) => {
-        createEle("green", ele, mgTop, mgLeft);
+        if (Array.isArray(ele)) {
+          console.log("inner 2", ele);
+          addArray(ele, mgTop, mgLeft);
+        } else {
+          if (typeof ele === "object") {
+            Object.entries(ele).forEach((element, idx) => {
+              console.log("inner 2 object", element);
+              addArray(element, mgTop, mgLeft);
+            });
+          } else {
+            mgLeft = parentMgLeft + 20;
+            createEle("green", ele, mgTop, mgLeft);
+            parentMgLeft = mgLeft - 20;
+
+            mgLeft = 0;
+          }
+        }
       });
+    } else {
+      if (typeof ele === "object") {
+        Object.entries(ele).forEach((element, idx) => {
+          if (Array.isArray(element)) {
+            element.map((ele) => {
+              console.log("outer 3", ele);
+              if (ele === "object") {
+                addArray(ele, mgTop, mgLeft);
+              } else if (Array.isArray(ele)) {
+                addArray(ele, mgTop, mgLeft);
+              } else {
+                mgLeft = parentMgLeft + 20;
+                createEle("orange", ele, mgTop, mgLeft);
+                parentMgLeft = mgLeft - 20;
+
+                mgLeft = 0;
+              }
+            });
+          } else {
+            console.log("outer object", element);
+            console.log("outer object type", typeof element);
+            // createEle("green", ele, mgTop, mgLeft);
+            addArray(ele, mgTop, mgLeft);
+          }
+        });
+      } else {
+      }
     }
+  } else {
+    if (Array.isArray(ele)) {
+      ele.map((ele) => {
+        // addArray(ele, mgTop, mgLeft);
+        createEle("purple", ele, mgTop, mgLeft);
+      });
+    } else {
+      mgLeft = parentMgLeft + 20;
+      createEle("red", ele, mgTop, mgLeft);
+      parentMgLeft = mgLeft;
+
+      mgLeft = 0;
+    }
+  }
+}
+
+function ifArray(ele) {
+  if (Array.isArray(ele)) {
   }
 }
 
@@ -64,15 +130,13 @@ function addObject(ele, mgTop, mgLeft) {
 // ---------- CREATE NEW HTML ELEMENT -------------
 function createEle(bgColor, data, mgTop, mgLeft) {
   let a = document.getElementById("as");
-  a.style.border = "1px solid black";
-  a.style.marginTop = "10px";
-  a.style.padding = "10px";
 
   var b = document.createElement("p");
   b.style.backgroundColor = bgColor;
   b.style.marginTop = "" + mgTop + "px";
   b.style.marginLeft = "" + mgLeft + "px";
-  b.style.padding = "10px";
+  b.style.padding = "5px";
+  b.style.fontSize = "10px";
   b.style.width = "max-content";
   b.style.color = "white";
 
@@ -81,3 +145,36 @@ function createEle(bgColor, data, mgTop, mgLeft) {
 
   a.appendChild(b);
 }
+
+//
+function createCanvas() {
+  let a = document.getElementById("as");
+
+  a.style.border = "1px solid black";
+  a.style.marginTop = "10px";
+  a.style.padding = "10px";
+}
+
+function reset() {
+  let a = document.getElementById("input");
+  document.getElementById("as").replaceChildren();
+  a.value = "";
+}
+// -------------- TESTING ----------------------
+
+let limit = false;
+let j = 0;
+function testloop() {
+  while (j != 10) {
+    j++;
+    if (j == 10) {
+      limit = true;
+    }
+    console.log("loop val", j);
+    console.log("---------------");
+    testloop(j);
+  }
+  return;
+}
+
+//-----------------------------------------------
